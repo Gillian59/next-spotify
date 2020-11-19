68934f2f5af7f97744492c3bfc6856f44026c376
LuggyG <loic.coryn@boulanger.com>
2020-11-19 18:06:11 +0100

    Style searchCard

--- src/components/resultat.tsx
+++ src/components/resultat.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { artist, album, track } from "../types/spotify.d";
+import { Row, Container } from "react-bootstrap";
+import styles from "../../styles/SearchItems.module.css";
+import { Card } from "react-bootstrap";
 
 type searchData = {
   artists: artist;
@@ -31,34 +34,47 @@ const ShowResultat: React.FC<any> = ({ accessToken, texteToFind }) => {
   if (data) {
     return (
       <div>
-        <p> artist</p>
-        {data.artists.items.map((elt: any) => {
-          return (
-            <div key={elt.name}>
-              {elt.images.length > 0 ? <img src={elt.images[0].url} alt={elt.name}></img> : null}
-              <p> {elt.name}</p>
-              <p>{elt.type}</p>
-            </div>
-          );
-        })}
-        <p>Titres</p>
-        {data.tracks.items.map((elt: any) => {
-          return (
-            <div key={elt.name}>
-              {elt.album.images.length > 0 ? <img src={elt.album.images[0].url} alt={elt.name}></img> : null}
-              <p> {elt.name}</p>
-            </div>
-          );
-        })}
-        <p>Albums</p>
-        {data.albums.items.map((elt: any) => {
-          return (
-            <div key={elt.name}>
-              {elt.images.length > 0 ? <img src={elt.images[0].url} alt={elt.name}></img> : null}
-              <p> {elt.name}</p>
-            </div>
-          );
-        })}
+        <Container>
+          <div>
+            <Row>
+              <p> Artists</p>
+              {data.artists.items.map((elt: any) => {
+                return (
+                  <Card className={styles.card + " col-12 col-md-3 col-lg-2 mx-2 mt-3"} key={elt.name}>
+                    {elt.images.length > 0 ? (
+                      <Card.Img className={styles.card_img + " img-fluid"} src={elt.images[0].url} alt={elt.name} />
+                    ) : null}
+                    <Card.Title className="title text-truncate" style={{ fontSize: "medium" }}>
+                      {elt.name}
+                    </Card.Title>
+                  </Card>
+                );
+              })}
+            </Row>
+          </div>
+          <Row>
+            <p>Titres</p>
+            {data.tracks.items.map((elt: any) => {
+              return (
+                <div key={elt.name}>
+                  {elt.album.images.length > 0 ? <img src={elt.album.images[0].url} alt={elt.name}></img> : null}
+                  <p> {elt.name}</p>
+                </div>
+              );
+            })}
+          </Row>
+          <Row>
+            <p>Albums</p>
+            {data.albums.items.map((elt: any) => {
+              return (
+                <div key={elt.name}>
+                  {elt.images.length > 0 ? <img src={elt.images[0].url} alt={elt.name}></img> : null}
+                  <p> {elt.name}</p>
+                </div>
+              );
+            })}
+          </Row>
+        </Container>
       </div>
     );
   } else {
