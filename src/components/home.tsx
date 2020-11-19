40365d589e566a164771561efc340622a0651c54
bkwiet <x.crecel@gmail.com>
2020-11-19 17:07:11 +0100

    Fix route on the way to ...

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,6 +4,7 @@ import PlaylistsCollection from "../components/playlists";
 const Home: React.FC = () => {
   return (
     <>
+      <h1>Welcome little listener</h1>
       <h3>Going to Drum And Bass</h3>
       <PlaylistsCollection playlistStyle={"dnb"} />
       <h3>Forge some Metal</h3>
@@ -13,7 +14,13 @@ const Home: React.FC = () => {
       <h3>Chill out with Reggae</h3>
       <PlaylistsCollection playlistStyle={"reggae"} />
       <h3>Up to be Happy</h3>
-      <PlaylistsCollection playlistStyle={"happy"} />​
+      <PlaylistsCollection playlistStyle={"happy"} />
+      <h3>Just heard a Bang ...</h3>
+      <PlaylistsCollection playlistStyle={"Bang"} />
+      <h3>Crazy banjo</h3>
+      <PlaylistsCollection playlistStyle={"Crazy banjo"} />
+      <h3>Did you know Pacman ?</h3>
+      <PlaylistsCollection playlistStyle={"Pacman"} />
     </>
   );
 };
